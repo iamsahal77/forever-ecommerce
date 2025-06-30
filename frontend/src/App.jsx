56a6c785d0929cd5188dc4fda5ab9f6e54f7bf1c
iamsahal77 <sahalsahsal2gmail@gmail.com>
2025-06-30 22:13:24 +0530

    feat(layout):add responsive navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/product/:productId' element={<Product/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/orders' element={<Orders/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div><h1>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab repudiandae voluptates ex dignissimos sunt itaque eaque, numquam vero? Necessitatibus hic recusandae quaerat placeat suscipit, molestias molestiae accusantium iusto ad quidem?</h1></div>
+  )
+}
+
+export default About
