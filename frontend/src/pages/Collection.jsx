d0405f9780bb174edfb8219dd5f4de2cad5bc4fc
iamsahal77 <sahalsahsal2gmail@gmail.com>
2025-07-04 12:28:23 +0530

    feat(server): setup Express server

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -87,7 +87,7 @@ const Collection = () => {
 
   return (
     <>
-      <SearchBar />
+      {/* <SearchBar /> */}
       <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
         {/* Filter Options */}
