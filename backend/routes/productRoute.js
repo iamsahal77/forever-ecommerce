2773a98b6eb69c03d7be7afdf141ad607dc46064
iamsahal77 <sahalsahsal2gmail@gmail.com>
2025-07-05 21:31:18 +0530

    feat(admin):complete admin panel

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,4 +1,4 @@
-import express from 'express'
+ import express from 'express'
 import { listProducts, addProduct, removeProduct, singleProduct } from '../controllers/productController.js'
 import upload from '../middleware/multer.js';
 import adminAuth from '../middleware/adminAuth.js';
